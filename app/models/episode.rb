1f8d9783261d90ffeacd4622411dc3bffc0463b4
Xavier Carty <xaviercarty@gmail.com>
2020-04-09 12:29:14 -0400

    done

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,12 @@
 class Episode < ApplicationRecord
+    has_many :appearances 
+    has_many :guests , through: :appearances
+
+    def average_rating 
+        if self.appearances.size > 0 
+            self.appearances.average(:rating)
+        else  
+            "undefined"
+        end 
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :guests, only: [:index]
-  resources :episodes, only: [:index]
+  resources :guests, only: [:index , :new , :show , :create , :update , :destroy]
+  resources :episodes, only: [:index , :new , :show , :create , :update , :destroy]
+  resources :appearances, only: [:index , :new , :create , :show ]
 end
